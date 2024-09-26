860748257bc67709b54a56ee3876b565ea1d29a5
jhirschv <johnhirschv@gmail.com>
2024-09-25 20:00:55 -0500

    added url for recommendations

--- FrontEnd/src/_root/pages/Product.jsx
+++ FrontEnd/src/_root/pages/Product.jsx
@@ -74,7 +74,7 @@ useEffect(() => {
 useEffect(() => {
     const fetchRecommendations = async () => {
       try {
-        const response = await axios.get(`http://127.0.0.1:5000/recommend/${productId}`);
+        const response = await axios.get(`https://besttech-machine-learning-6e9853bfcfbd.herokuapp.com/recommend/${productId}`);
         setRecommendations(response.data);
         console.log(response.data)
       } catch (error) {
